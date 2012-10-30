48b7ac5f9d6b50f2c41abed4a0a3337a979d2278
cordell <cordellsmail@gmail.com>
2012-10-30 06:14:34 -0500

    Update slim.mk
    back to CM inline kernel

--- slim.mk
+++ slim.mk
@@ -12,24 +12,6 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 # Inherit device settings
 $(call inherit-product, vendor/slim/config/common_sgs.mk)
 
-#copy kernel and modules
-PRODUCT_COPY_FILES += \
-device/samsung/d2vzw/prebuilt/boot.img:system/slimkernel/boot.img \
-device/samsung/d2vzw/prebuilt/kernel:kernel \
-device/samsung/d2vzw/prebuilt/system/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/qce40.ko:system/lib/modules/qce40.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/spidev.ko:system/lib/modules/spidev.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/qcrypto.ko:system/lib/modules/qcrypto.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/qcedev.ko:system/lib/modules/qcedev.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/vpnclient.ko:system/lib/modules/vpnclient.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/gspca_main.ko:system/lib/modules/gpsca_main.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/btlock.ko:system/lib/modules/btlock.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/dhd.ko:system/lib/modules/dhd.ko \
-device/samsung/d2vzw/prebuilt/system/lib/modules/evbug.ko:system/lib/modules/evbug.ko \
-device/samsung/d2vzw/prebuilt/system/bin/thermald:system/bin/thermald \
-device/samsung/d2vzw/prebuilt/system/xbin/ffc:system/xbin/ffc \
-device/samsung/d2vzw/prebuilt/system/xbin/stocktherm:system/xbin/stocktherm
-
 #copy 00check
 PRODUCT_COPY_FILES += \
 	vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
