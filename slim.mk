cc7245fa34e2d8910c7d7cbb97292baa4a93aaa9
cordell <cordellsmail@gmail.com>
2012-12-13 16:51:24 -0600

    d2vzw - remove CM and add Slim
    Change-Id: Ia6cfe2f315e9e669abaab72b3628b2a5c92f1d0d

---
+++ slim.mk
@@ -0,0 +1,30 @@
+$(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := d2vzw
+
+# Inherit some common SLIM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device settings
+$(call inherit-product, vendor/slim/config/common_sgs.mk)
+
+# Inherit torch settings
+$(call inherit-product, vendor/slim/config/common_ledflash.mk)
+
+#copy 00check
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+
+#bootanimation
+PRODUCT_COPY_FILES +=  \
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="d2vzw-user 4.1.1 JRO03L I535VRBLK3 release-keys" PRIVATE_BUILD_DESC="Verizon/d2vzw/d2vzw:4.1.1/JRO03L/I535VRBL
+K3:user/release-keys"
+
+PRODUCT_NAME := slim_d2vzw
+PRODUCT_DEVICE := d2vzw
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SCH-I535
+PRODUCT_MANUFACTURER := Samsung
