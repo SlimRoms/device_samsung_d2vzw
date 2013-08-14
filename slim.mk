4d7e14c317fea2ab2e46f7a530287bddf8146da7
nicklovell23 <nicklovell23@gmail.com>
2013-08-14 05:25:06 +0200

    Adjust slim.mk for new bootanimation
    Change-Id: I0f523de13a7489edffa7577f34ee3f72d82036d3

--- slim.mk
+++ slim.mk
@@ -15,10 +15,6 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
-#bootanimation
-PRODUCT_COPY_FILES +=  \
-    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
 
 PRODUCT_NAME := slim_d2vzw
