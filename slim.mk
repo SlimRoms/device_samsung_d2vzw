7e8cba978e82f334457bf495248d9ad6a4185ab6
nicklovell23 <nicklovell23@gmail.com>
2013-12-25 06:26:09 +0100

    Remove common led flash
    Change-Id: I34b1c7c86d94a72cf2510b19f4e608833c665f5b

--- slim.mk
+++ slim.mk
@@ -9,9 +9,6 @@ $(call inherit-product, vendor/slim/config/cdma.mk)
 # Inherit some common SLIM stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
-# Inherit torch settings
-$(call inherit-product, vendor/slim/config/common_ledflash.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2vzw TARGET_DEVICE=d2vzw BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.1.2/JZO54K/I535VRBMF1:user/release-keys" PRIVATE_BUILD_DESC="d2vzw-user 4.1.2 JZO54K I535VRBMF1 release-keys"
 
 PRODUCT_NAME := slim_d2vzw
