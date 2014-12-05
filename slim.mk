3c1e8282222dad6534298c602b3195ca6140e912
nicklovell23 <nicklovell23@gmail.com>
2014-12-05 21:55:45 +0100

    Perseus71 LP Bringup

--- slim.mk
+++ slim.mk
@@ -1,23 +1,18 @@
 $(call inherit-product, device/samsung/d2vzw/full_d2vzw.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := d2vzw
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
-# Inherit some common SLIM stuff.
-$(call inherit-product, vendor/slim/config/cdma.mk)
-
-# Inherit some common SLIM stuff.
+# Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2vzw \
     TARGET_DEVICE=d2vzw \
-    BUILD_FINGERPRINT="Verizon/d2vzw/d2vzw:4.3/JSS15J/I535VRUCML1:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2vzw-user 4.3 JSS15J I535VRUCML1 release-keys"
+    BUILD_FINGERPRINT="samsung/d2vzw/d2vzw:4.4.2/KOT49H/L710VPUDND8:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2vzw-user 4.4.2 KOT49H I747UCUFNE4 release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 PRODUCT_NAME := slim_d2vzw
 PRODUCT_DEVICE := d2vzw
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SCH-I535
-PRODUCT_MANUFACTURER := Samsung
-
